f9b4453ed65ea560cdefa98a29047323a2dcec4c
Jeferson Menezes <keepcoolerbr@gmail.com>
2017-09-07 15:58:15 -0300

    Exercicio2

---
+++ Aplication/Program.cs
@@ -0,0 +1,31 @@
+﻿using Stream;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua stream: ");
+            StreamString obj = new StreamString(Console.ReadLine());
+
+            char c = obj.firstChar(obj);
+
+            if (c != ' ')
+            {
+                Console.WriteLine("Caractere encontrado: " + c);
+            }
+            else
+            {
+                Console.WriteLine("Nenhum caractere encontrado");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Testes/TesteStream.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stream;
+
+namespace Testes
+{
+    [TestClass]
+    public class TesteStream
+    {
+        [TestMethod]
+        public void TestVogalValid()
+        {
+            StreamString stream = new StreamString("aAbBABacfe");
+            char resultado;
+
+            try
+            {
+                resultado = stream.firstChar(stream);
+            }
+            catch (System.Exception)
+            {
+                resultado = ' ';
+            }
+            Assert.IsTrue(resultado != ' ');
+        }
+
+        [TestMethod]
+        public void TestVogalInvalid()
+        {
+            StreamString stream = new StreamString("aaaaaaaaa");
+            char resultado;
+
+            try
+            {
+                resultado = stream.firstChar(stream);
+            }
+            catch (System.Exception)
+            {
+                resultado = ' ';
+            }
+            Assert.IsTrue(resultado == ' ');
+        }
+    }
+}
