49e25492f333f4e31a2578c612925b2b85097eb7
Jeferson Menezes <keepcoolerbr@gmail.com>
2017-09-07 16:07:32 -0300

    Exercicio2

--- Aplication/Program.cs
+++ Aplication/Program.cs
@@ -1,9 +1,9 @@
-﻿using Stream;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StringStream;
 
 namespace Aplication
 {
